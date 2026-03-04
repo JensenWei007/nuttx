e6bb77ed106eaf270b0614915994a33dc71b3ed7
JensenWei007 <jensenwei007@gmail.com>
2026-03-04 16:31:20 +0800

    createstack
    Signed-off-by: JensenWei007 <jensenwei007@gmail.com>

--- arch/rh850/src/common/rh850_createstack.c
+++ arch/rh850/src/common/rh850_createstack.c
@@ -40,6 +40,8 @@
 #include <nuttx/syslog/syslog.h>
 #include <arch/board/board.h>
 
+#include "rh850_internal.h"
+
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -84,7 +86,124 @@
 
 int up_create_stack(struct tcb_s *tcb, size_t stack_size, uint8_t ttype)
 {
-    early_syslog("up_create_stack");
+#ifdef CONFIG_TLS_ALIGNED
+  /* The allocated stack size must not exceed the maximum possible for the
+   * TLS feature.
+   */
 
-  return 1;
+  DEBUGASSERT(stack_size <= TLS_MAXSTACK);
+  if (stack_size >= TLS_MAXSTACK)
+  {
+    stack_size = TLS_MAXSTACK;
+  }
+#endif
+
+  /* Is there already a stack allocated of a different size?  Because of
+   * alignment issues, stack_size might erroneously appear to be of a
+   * different size.  Fortunately, this is not a critical operation.
+   */
+
+  if (tcb->stack_alloc_ptr && tcb->adj_stack_size != stack_size)
+  {
+    /* Yes.. Release the old stack */
+
+    up_release_stack(tcb, ttype);
+  }
+
+  /* Do we need to allocate a new stack? */
+
+  if (!tcb->stack_alloc_ptr)
+  {
+    /* Allocate the stack.  If DEBUG is enabled (but not stack debug),
+     * then create a zeroed stack to make stack dumps easier to trace.
+     * If TLS is enabled, then we must allocate aligned stacks.
+     */
+
+#ifdef CONFIG_TLS_ALIGNED
+#ifdef CONFIG_MM_KERNEL_HEAP
+    /* Use the kernel allocator if this is a kernel thread */
+
+    if (ttype == TCB_FLAG_TTYPE_KERNEL)
+    {
+      tcb->stack_alloc_ptr = kmm_memalign(TLS_STACK_ALIGN, stack_size);
+    }
+    else
+#endif
+    {
+      /* Use the user-space allocator if this is a task or pthread */
+
+      tcb->stack_alloc_ptr = kumm_memalign(TLS_STACK_ALIGN, stack_size);
+    }
+
+#else /* CONFIG_TLS_ALIGNED */
+#ifdef CONFIG_MM_KERNEL_HEAP
+    /* Use the kernel allocator if this is a kernel thread */
+
+    if (ttype == TCB_FLAG_TTYPE_KERNEL)
+    {
+      tcb->stack_alloc_ptr = kmm_malloc(stack_size);
+    }
+    else
+#endif
+    {
+      /* Use the user-space allocator if this is a task or pthread */
+
+      tcb->stack_alloc_ptr = kumm_malloc(stack_size);
+    }
+#endif /* CONFIG_TLS_ALIGNED */
+
+#ifdef CONFIG_DEBUG_FEATURES
+    /* Was the allocation successful? */
+
+    if (!tcb->stack_alloc_ptr)
+    {
+      serr("ERROR: Failed to allocate stack, size %zd\n", stack_size);
+    }
+#endif
+  }
+
+  /* Did we successfully allocate a stack? */
+
+  if (tcb->stack_alloc_ptr)
+  {
+    uintptr_t top_of_stack;
+    size_t size_of_stack;
+
+    /* Yes.. If stack debug is enabled, then fill the stack with a
+     * recognizable value that we can use later to test for high
+     * water marks.
+     */
+
+#ifdef CONFIG_STACK_COLORATION
+    memset(tcb->stack_alloc_ptr, 0xaa, stack_size);
+#endif
+
+    /* The SH family uses a push-down stack:  the stack grows
+     * toward loweraddresses in memory.  The stack pointer
+     * register, points to the lowest, valid work address
+     * (the "top" of the stack).  Items on the stack are
+     * referenced as positive word offsets from sp.
+     */
+
+    top_of_stack = (uintptr_t)tcb->stack_alloc_ptr + stack_size;
+
+    /* The SH stack must be aligned at word (4 byte)
+     * boundaries. If necessary top_of_stack must be rounded
+     * down to the next boundary
+     */
+
+    top_of_stack &= ~3;
+    size_of_stack = top_of_stack - (uintptr_t)tcb->stack_alloc_ptr;
+
+    /* Save the adjusted stack values in the struct tcb_s */
+
+    tcb->stack_base_ptr = tcb->stack_alloc_ptr;
+    tcb->adj_stack_size = size_of_stack;
+    tcb->flags |= TCB_FLAG_FREE_STACK;
+
+    //board_autoled_on(LED_STACKCREATED);
+    return OK;
+  }
+
+  return ERROR;
 }
